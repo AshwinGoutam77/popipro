6eca24e90c8b6e6944f497c701307aaf6da5b289
AshwinGoutam77 <ashwini@popipro.com>
2023-10-30 17:35:18 +0530

    bug fix in sitemap again

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,7 +1,7 @@
 export default function sitemap() {
   let urls = fetch(`https://admin.popipro.com/api/published-url`);
 
-  let profiles = urls.data.map((url) => {
+  let profiles = urls?.data?.map((url) => {
     return {
       key: url,
       url: "https://work.popipro.com/" + url,
