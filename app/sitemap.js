651ea696e46aa7066f9a3f5ac3f5924582955bff
AshwinGoutam77 <ashwini@popipro.com>
2023-10-30 17:29:05 +0530

    live api add in sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,9 +1,10 @@
 export default function sitemap() {
-  let urls = fetch(`https://jsonplaceholder.typicode.com/posts`);
+  let urls = fetch(`https://admin.popipro.com/api/published-url`);
 
-  let profiles = urls.map((url, i) => {
+  let profiles = urls.map((url) => {
     return {
-      url: 'https://work.popipro.com/'+url.userId,
+      key: url,
+      url: "https://work.popipro.com/" + url,
       lastModified: new Date().toISOString(),
     };
   });
