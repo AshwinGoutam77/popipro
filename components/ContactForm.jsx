5d71913f68f7e51b132098ec96f0f4f3a05e05aa
sureshprajapat95 <80397895+sureshprajapat95@users.noreply.github.com>
2023-11-02 15:10:51 +0530

    fix full width issue of date and time picker

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -133,35 +133,37 @@ export default function ContactForm({ card_url, Titles, PlanData, MainData }) {
               />
               <div className="help-block with-errors"></div>
             </div>
-            <div className="row">
-              {MainData?.company_setting?.show_appointment_button !== 0 ? (
-                <div className="form-group col-lg-6 col-md-6 col-12 mb-2">
-                  <input
-                    type="date"
-                    value={Date}
-                    onChange={(e) => setDate(e.target.value)}
-                    className="date-time-input"
-                    style={{ minWidth: "100%" }}
-                  />
-                  <div className="help-block with-errors"></div>
-                </div>
-              ) : (
-                ""
-              )}
-              {MainData?.company_setting?.show_appointment_button !== 0 ? (
-                <div className="form-group col-lg-6 col-md-6 col-12 mb-2">
-                  <input
-                    type="time"
-                    value={Time}
-                    onChange={(e) => setTime(e.target.value)}
-                    className="date-time-input"
-                    style={{ minWidth: "100%" }}
-                  />
-                  <div className="help-block with-errors"></div>
-                </div>
-              ) : (
-                ""
-              )}
+            <div className="col-12">
+              <div className="row">
+                {MainData?.company_setting?.show_appointment_button !== 0 ? (
+                  <div className="col-lg-6 col-md-6 mb-2">
+                    <input
+                      type="date"
+                      value={Date}
+                      onChange={(e) => setDate(e.target.value)}
+                      className="date-time-input"
+                      style={{ minWidth: "95%", borderRadius: 0 }}
+                    />
+                    <div className="help-block with-errors"></div>
+                  </div>
+                ) : (
+                  ""
+                )}
+                {MainData?.company_setting?.show_appointment_button !== 0 ? (
+                  <div className="col-lg-6 col-md-6 mb-2 text-center">
+                    <input
+                      type="time"
+                      value={Time}
+                      onChange={(e) => setTime(e.target.value)}
+                      className="date-time-input"
+                      style={{ minWidth: "95%", borderRadius: 0 }}
+                    />
+                    <div className="help-block with-errors"></div>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
             </div>
 
             <div className="form-group col-lg-12 col-md-12 mb-2">
