f1ba5a6201cea69f12771ea746cc79e49e14b3dd
sureshprajapat95 <80397895+sureshprajapat95@users.noreply.github.com>
2023-11-02 14:45:09 +0530

    try full width on datepicker

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -154,7 +154,7 @@ export default function ContactForm({ card_url, Titles, PlanData, MainData }) {
                   value={Time}
                   onChange={(e) => setTime(e.target.value)}
                   className="date-time-input"
-                  style={{ display: "-webkit-inline-box" }}
+                  style={{ minWidth: '100%' }}
                 />
                 <div className="help-block with-errors"></div>
               </div>
