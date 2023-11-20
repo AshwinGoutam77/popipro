46132175fb51a7631926adf247ccacc71fc340c1
AshwinGoutam77 <ashwini@popipro.com>
2023-11-20 11:36:09 +0530

    3.0

--- app/sitemap.js
+++ app/sitemap.js
@@ -3,9 +3,8 @@ export default async function sitemap() {
   let urls = await data.json();
   let profiles = urls?.data?.map((url) => {
     return {
-      key: url?.url,
-      url: "https://app.popipro.com/" + url?.url,
-      lastModified: url?.date,
+      key: 1,
+      url: "https://app.popipro.com/prafull-gupta"
     };
   });
 
