eb68cb24ff6f2aba4dc45d55b3502e22c4c812e6
AshwinGoutam77 <ashwini@popipro.com>
2023-11-20 11:08:41 +0530

    robot.txt file code

--- app/robots.js
+++ app/robots.js
@@ -1,22 +1,36 @@
+let MODE = process.env.NEXT_PUBLIC_MODE;
 export default function robots() {
-  return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: [
-        "/login/",
-        "/dashboard/",
-        "/insights/",
-        "/leads/",
-        "/plan/",
-        "/products/",
-        "/product-enquiry/",
-        "/testimonialsLeads/",
-        "/blog/",
-        "/appointment-lead/",
-        "/edit/",
-      ],
-    },
-    sitemap: "https://app.popipro.com/sitemap.xml",
-  };
+  let robots_obj;
+
+  if(MODE !== 'production') {
+    robots_obj = {
+      rules: {
+        disallow: '/',
+      }
+    };
+  }else{
+    robots_obj = {
+      rules: {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/login/",
+          "/dashboard/",
+          "/insights/",
+          "/leads/",
+          "/plan/",
+          "/products/",
+          "/product-enquiry/",
+          "/testimonialsLeads/",
+          "/blog/",
+          "/appointment-lead/",
+          "/edit/",
+        ],
+      },
+      sitemap: "https://app.popipro.com/sitemap.xml",
+    };
+  }
+
+
+  return robots_obj;
 }
