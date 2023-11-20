b07d39874a820c4be8d89bc678aaea68bfeacf2a
AshwinGoutam77 <ashwini@popipro.com>
2023-11-20 11:22:42 +0530

    sitemap issue

--- app/sitemap.js
+++ app/sitemap.js
@@ -2,6 +2,7 @@ export default async function sitemap() {
   let data = await fetch(`https://admin.popipro.com/api/published-url`);
   let urls = await data.json();
   let profiles = urls?.data?.map((url) => {
+    console.log(url.url);
     return {
       key: url,
       url: "https://app.popipro.com/" + url.url,
