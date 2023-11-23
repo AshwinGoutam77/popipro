cf4a950e7fff1d5cd642a0728d429f12bafdb42b
ashwin <ashwin@gmail.com>
2023-11-23 09:22:54 +0530

    signature changes

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -487,7 +487,7 @@ export default function Dashboard() {
             </div>
 
             {/* Signature */}
-            <div className="col-6 col-lg-3 mt-3 d-flex justify-content-center p-0 px-2">
+            {/* <div className="col-6 col-lg-3 mt-3 d-flex justify-content-center p-0 px-2">
               <Link href="/signature" className="w-100">
                 <div className="dashboard-boxes d-flex justify-content-center align-items-center flex-column">
                   <FontAwesomeIcon
@@ -500,7 +500,7 @@ export default function Dashboard() {
                   </h6>
                 </div>
               </Link>
-            </div>
+            </div> */}
           </div>
 
           {/* ........... */}
