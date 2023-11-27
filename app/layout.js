1d50e3a6c6cbd1908b6aa19739a4fd2c9a6e25eb
AshwinGoutam77 <ashwini@popipro.com>
2023-11-27 13:08:34 +0530

    head code

--- app/layout.js
+++ app/layout.js
@@ -25,13 +25,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {process.env.NEXT_PUBLIC_MODE !== "development" ? (
+        {process.env.NEXT_PUBLIC_MODE !== "development" && (
           <meta
             name="google-site-verification"
             content="0WVIx5gDG1k-9nYt0gHCFJPjrlYeRIjIr2asTHblQcQ"
           />
-        ) : (
-          ""
         )}
       </head>
       <Script src="https://www.googletagmanager.com/gtag/js?id=G-JGP54SPWQQ" />
