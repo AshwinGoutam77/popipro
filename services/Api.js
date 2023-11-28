10cc1be3c95ee0bdbb448d957b5e1cb75271bf96
AshwinGoutam77 <ashwini@popipro.com>
2023-11-28 11:54:37 +0530

    ...

--- services/Api.js
+++ services/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect } from "react";
 
 const baseURL =
-  process.env.NEXT_PUBLIC_MODE == "developement"
+  process.env.NEXT_PUBLIC_MODE == "development"
     ? "https://dev.popipro.com/api/"
     : "https://admin.popipro.com/api/";
 
