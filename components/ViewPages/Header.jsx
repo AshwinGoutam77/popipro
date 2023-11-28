f2a22e6ff9ef833cc7bddc995d29022906499d70
AshwinGoutam77 <ashwini@popipro.com>
2023-11-28 15:34:51 +0530

    appointment code

--- components/ViewPages/Header.jsx
+++ components/ViewPages/Header.jsx
@@ -899,7 +899,7 @@ const Header = ({
                 </button>
               )}
               {Titles?.card_booking?.is_active == 0 ||
-              PlanData?.is_expired !== false ? (
+              PlanData?.is_expired !== false || card?.id !== "S7ZG"? (
                 ""
               ) : (
                 <button
