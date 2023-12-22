6b2e13c33527f91d14f248484231c9db5caf862b
AshwinGoutam77 <ashwini@popipro.com>
2023-12-22 18:51:18 +0530

    live the code

--- components/EditPages/EditProducts.jsx
+++ components/EditPages/EditProducts.jsx
@@ -916,7 +916,7 @@ export default function EditProducts({
                     style={{ height: "40px", border: "1px solid #ccc" }}
                     onChange={(e) => setServicesName(e.target.value)}
                   ></input>
-                  {items?.label == null? (
+                  {items?.is_label == 0 ? (
                     <>
                       <label className="modalFormLable">Price</label>
                       <div className="d-flex" style={{ gap: "10px" }}>
