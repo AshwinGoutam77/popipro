f06e1a15b57f581a47180dd9a6b63938cf5dae75
AshwinGoutam77 <ashwini@popipro.com>
2023-12-25 13:13:28 +0530

    live7

--- app/insights/page.jsx
+++ app/insights/page.jsx
@@ -734,7 +734,7 @@ const Insights = () => {
 
                           <div className="card -mt-5 w-full rounded-2xl px-3 py-3 text-center">
                             <p className="mt-3 text-base font-medium text-slate-700 dark:text-navy-100 font-weight-bold">
-                              {item.label}
+                              {item?.label}
                             </p>
                             <a
                               href="#"
