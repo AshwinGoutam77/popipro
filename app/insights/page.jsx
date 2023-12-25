caf125cab7cf52f47fe2d0893a0450469d052a18
AshwinGoutam77 <ashwini@popipro.com>
2023-12-25 14:03:14 +0530

    live17

--- app/insights/page.jsx
+++ app/insights/page.jsx
@@ -28,6 +28,7 @@ import { redirect } from "next/navigation";
 import { useAuthContext } from "@context/AuthContext";
 import { Chart, registerables } from "chart.js";
 Chart.register(...registerables);
+import ReactApexChart from "react-apexcharts";
 
 const Insights = () => {
   const { token } = useAuthContext();
@@ -129,6 +130,118 @@ const Insights = () => {
         theme: "light",
       });
     }
+  };
+
+  let dSet =
+    Data &&
+    Data?.click_hits?.social_media?.map((item) => {
+      return {
+        name: item?.name,
+        data: item?.data,
+      };
+    });
+  const chartData = {
+    series: [
+      {
+        name: "Total Profile Views",
+        data: Data?.click_hits?.hits ? Data?.click_hits?.hits : "",
+      },
+      {
+        name: "Total Save Contacts",
+        data: Data?.click_hits?.saved_contact
+          ? Data?.click_hits?.saved_contact
+          : "",
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: "area",
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: "smooth",
+      },
+      xaxis: {
+        type: "month",
+        categories: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ],
+      },
+      tooltip: {
+        x: {
+          format: "dd/MM/yy HH:mm",
+        },
+      },
+    },
+  };
+  const chartData2 = {
+    series: dSet || [],
+    options: {
+      chart: {
+        type: "bar",
+        height: 350,
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: "55%",
+          endingShape: "rounded",
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        show: true,
+        width: 2,
+        colors: ["transparent"],
+      },
+      xaxis: {
+        categories: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ],
+      },
+      yaxis: {
+        title: {
+          text: "$ (thousands)",
+        },
+      },
+      fill: {
+        opacity: 2,
+      },
+      tooltip: {
+        y: {
+          formatter: function (val) {
+            return "$ " + val + " thousands";
+          },
+        },
+      },
+    },
   };
 
   return token ? (
@@ -262,7 +375,7 @@ const Insights = () => {
 
             {/* Chart */}
 
-            {/* <div className="row w-100 m-0">
+            <div className="row w-100 m-0">
               <div className="col-12 col-lg-6 mt-4 px-0">
                 <div className="barchart-div mx-4">
                   <div id="chart">
@@ -289,14 +402,14 @@ const Insights = () => {
               ) : (
                 ""
               )}
-            </div> */}
+            </div>
 
             {/* Product */}
 
             <h5 className="first-title title__separate mx-4 mt-4 text-black">
               {UserData?.titles?.card_products?.visible_name} Analytics
             </h5>
-            <div className="mx-4 mb-4">
+            <div className="mx-2 mb-4">
               <div className="row m-0">
                 <div className="col-12 col-lg-3 margin-sm-top">
                   <div className="card p-4">
