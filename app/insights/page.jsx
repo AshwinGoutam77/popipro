57108bbc7d96180c7c3f1057dd0dd6e3afe0354e
AshwinGoutam77 <ashwini@popipro.com>
2023-12-25 12:54:56 +0530

    live2

--- app/insights/page.jsx
+++ app/insights/page.jsx
@@ -23,6 +23,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import Api from "@services/Api";
 import { EditData, GetInshights } from "@services/Routes";
 import "../../styles/about.css";
+import "../styles/graph.css";
 import { redirect } from "next/navigation";
 import { useAuthContext } from "@context/AuthContext";
 import { Chart, registerables } from "chart.js";
@@ -235,72 +236,74 @@ const Insights = () => {
             <h5 className="first-title title__separate mx-4  text-black">
               Quick Analytics
             </h5>
-            <div className="mx-2">
-              <div className="row mt-4 px-2 w-100 mx-0" style={{ gap: "0px" }}>
-                <div className="col-6 col-lg-3 mb-2 p-0 px-1">
-                  <div className="text-left insights-dashboard-boxes">
-                    <div className="d-flex align-items-center mb-1">
-                      <FontAwesomeIcon
-                        icon={faEye}
-                        className="text-white mr-2"
-                        width="20"
-                      />
-                      <p className="text-white font-weight-bold">
-                        Profile Views
-                      </p>
-                    </div>
-                    <h5 className="text-white ml-4">
-                      {Data?.total_click_hits}
-                    </h5>
-                  </div>
+            <div className="mt-5 grid grid-cols-2 gap-4 px-4 sm:grid-cols-4 sm:px-5">
+              <div className="relative flex flex-col overflow-hidden rounded-lg bg-gradient-to-br from-pink-500 to-rose-500 p-3.5">
+                <p className="text-xs font-weight-bold text-pink-100">
+                  Profile Views
+                </p>
+                <div className="flex items-end justify-between space-x-2">
+                  <p className="mt-4 text-2xl font-medium text-white">
+                    {Data?.total_click_hits}
+                  </p>
+                  <a
+                    href="#"
+                    className="border-b border-left-0 border-right-0 border-top-0 border-dotted border-current pb-0.5 text-xs font-medium text-pink-100 outline-none transition-colors duration-300 line-clamp-1 hover:text-white focus:text-white"
+                  >
+                    Get Report
+                  </a>
                 </div>
-                <div className="col-6 col-lg-3 mb-2 p-0 px-1">
-                  <div className="text-left insights-dashboard-boxes">
-                    <div className="d-flex align-items-center mb-1">
-                      <FontAwesomeIcon
-                        icon={faDownload}
-                        className="text-white mr-2"
-                        width="20"
-                      />
-                      <p className="text- font-weight-bold">Save Contacts</p>
-                    </div>
-                    <h5 className="text-white ml-4">
-                      {Data?.total_saved_contact}
-                    </h5>
-                  </div>
+                <div className="mask is-hexagon-2 absolute top-0 right-0 -m-3 h-16 w-16 bg-white/20"></div>
+              </div>
+              <div className="relative flex flex-col overflow-hidden rounded-lg bg-gradient-to-br from-info to-info-focus p-3.5">
+                <p className="text-xs font-weight-bold text-sky-100">
+                  Save Contacts
+                </p>
+                <div className="flex items-end justify-between space-x-2">
+                  <p className="mt-4 text-2xl font-medium text-white">
+                    {Data?.total_saved_contact}
+                  </p>
+                  <a
+                    href="#"
+                    className="border-b border-left-0 border-right-0 border-top-0 border-dotted border-current pb-0.5 text-xs font-medium text-sky-100 outline-none transition-colors duration-300 line-clamp-1 hover:text-white focus:text-white"
+                  >
+                    Get Report
+                  </a>
                 </div>
-                <div className="col-6 col-lg-3 mb-2 p-0 px-1">
-                  <div className="text-left insights-dashboard-boxes">
-                    <div className="d-flex align-items-center mb-1">
-                      <FontAwesomeIcon
-                        icon={faBagShopping}
-                        className="text-white mr-2"
-                        width="20"
-                      />
-                      <p className="text-white font-weight-bold">
-                        {UserData?.titles?.card_products?.visible_name} Views
-                      </p>
-                    </div>
-                    <h5 className="text-white ml-4">
-                      {Data?.card_states?.product_views}
-                    </h5>
-                  </div>
+                <div className="mask is-reuleaux-triangle absolute top-0 right-0 -m-3 h-16 w-16 bg-white/20"></div>
+              </div>
+              <div className="relative flex flex-col overflow-hidden rounded-lg bg-gradient-to-br from-amber-400 to-orange-600 p-3.5">
+                <p className="text-xs font-weight-bold text-amber-50">
+                  {UserData?.titles?.card_products?.visible_name} Views
+                </p>
+                <div className="flex items-end justify-between space-x-2">
+                  <p className="mt-4 text-2xl font-medium text-white">
+                    {Data?.card_states?.product_views}
+                  </p>
+                  <a
+                    href="#"
+                    className="border-b border-left-0 border-right-0 border-top-0 border-dotted border-current pb-0.5 text-xs font-medium text-amber-50 outline-none transition-colors duration-300 line-clamp-1 hover:text-white focus:text-white"
+                  >
+                    Get Report
+                  </a>
                 </div>
-                <div className="col-6 col-lg-3 mb-2 p-0 px-1">
-                  <div className="text-left insights-dashboard-boxes">
-                    <div className="d-flex align-items-center mb-1">
-                      <FontAwesomeIcon
-                        icon={faShare}
-                        className="text-white mr-2"
-                        width="20"
-                      />
-                      <p className="text-white font-weight-bold">Your Leads</p>
-                    </div>
-                    <h5 className="text-white ml-4">
-                      {Data?.total_share_contact}
-                    </h5>
-                  </div>
+                <div className="mask is-diamond absolute top-0 right-0 -m-3 h-16 w-16 bg-white/20"></div>
+              </div>
+              <div className="relative flex flex-col overflow-hidden rounded-lg bg-gradient-to-br from-info to-info-focus p-3.5">
+                <p className="text-xs font-weight-bold text-amber-50">
+                  Your Leads
+                </p>
+                <div className="flex items-end justify-between space-x-2">
+                  <p className="mt-4 text-2xl font-medium text-white">
+                    {Data?.total_share_contact}
+                  </p>
+                  <a
+                    href="#"
+                    className="border-b border-left-0 border-right-0 border-top-0 border-dotted border-current pb-0.5 text-xs font-medium text-amber-50 outline-none transition-colors duration-300 line-clamp-1 hover:text-white focus:text-white"
+                  >
+                    Get Report
+                  </a>
                 </div>
+                <div className="mask is-diamond absolute top-0 right-0 -m-3 h-16 w-16 bg-white/20"></div>
               </div>
             </div>
 
