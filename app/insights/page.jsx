b39c9dff40fded08aff67216690be2cc5b1ec57b
AshwinGoutam77 <ashwini@popipro.com>
2023-12-25 14:07:34 +0530

    live18

--- app/insights/page.jsx
+++ app/insights/page.jsx
@@ -378,26 +378,34 @@ const Insights = () => {
             <div className="row w-100 m-0">
               <div className="col-12 col-lg-6 mt-4 px-0">
                 <div className="barchart-div mx-4">
-                  <div id="chart">
-                    <ReactApexChart
-                      options={chartData?.options}
-                      series={chartData?.series}
-                      type="area"
-                      height={350}
-                    />
-                  </div>
+                  {chartData ? (
+                    <div id="chart">
+                      <ReactApexChart
+                        options={chartData?.options}
+                        series={chartData?.series}
+                        type="area"
+                        height={350}
+                      />
+                    </div>
+                  ) : (
+                    ""
+                  )}
                 </div>
               </div>
               {Data?.users_social_link?.length !== 0 ? (
                 <div className="col-12 col-lg-6 mt-4 px-0">
-                  <div className="barchart-div mx-4">
-                    <ReactApexChart
-                      options={chartData2?.options}
-                      series={chartData2?.series}
-                      type="bar"
-                      height={350}
-                    />
-                  </div>
+                  {chartData2 ? (
+                    <div className="barchart-div mx-4">
+                      <ReactApexChart
+                        options={chartData2?.options}
+                        series={chartData2?.series}
+                        type="bar"
+                        height={350}
+                      />
+                    </div>
+                  ) : (
+                    ""
+                  )}
                 </div>
               ) : (
                 ""
