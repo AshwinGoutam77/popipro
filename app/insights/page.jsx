4b603e9b972dcf619069a49a7f97de109a8b928c
AshwinGoutam77 <ashwini@popipro.com>
2023-12-25 13:22:22 +0530

    live9

--- app/insights/page.jsx
+++ app/insights/page.jsx
@@ -28,7 +28,6 @@ import { redirect } from "next/navigation";
 import { useAuthContext } from "@context/AuthContext";
 import { Chart, registerables } from "chart.js";
 Chart.register(...registerables);
-import ReactApexChart from "react-apexcharts";
 
 const Insights = () => {
   const { token } = useAuthContext();
