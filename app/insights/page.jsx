4b7f982fc5d757b3a7e843bcacc626db5c3bad5c
AshwinGoutam77 <ashwini@popipro.com>
2023-12-25 12:34:31 +0530

    live

--- app/insights/page.jsx
+++ app/insights/page.jsx
@@ -55,26 +55,29 @@ const Insights = () => {
       if (response.data.status) {
         setShowLoader(false);
         setUserData(response.data.data);
-        document.documentElement.style.setProperty(
-          "--color",
-          response.data.data.card.color_code
-        );
-        document.documentElement.style.setProperty(
-          "--themecolor",
-          response.data.data.card.background_color
-        );
-        const color = getComputedStyle(
-          document.documentElement
-        ).getPropertyValue("--color");
+        // document.documentElement.style.setProperty(
+        //   "--color",
+        //   response.data.data.card.color_code
+        // );
+        // document.documentElement.style.setProperty(
+        //   "--themecolor",
+        //   response.data.data.card.background_color
+        // );
+        // const color = getComputedStyle(
+        //   document.documentElement
+        // ).getPropertyValue("--color");
       }
     } catch (error) {
-      if (error.request.status == "401" && typeof window !== "undefined") {
-        localStorage.removeItem("token");
-        window.location.href = "/login";
+      if (error.request.status == "401") {
+        // localStorage.removeItem("token");
+        // window.location.href = "/login";
+        console.log(error);
       }
     }
     setShowLoader(false);
   };
+
+
   const api = async () => {
     setShowLoader(true);
     const response = await Api(GetInshights, {});
@@ -124,9 +127,10 @@ const Insights = () => {
         setShowLoader(false);
       }
     } catch (error) {
-      if (error.request.status == "401" && typeof window !== "undefined") {
-        localStorage.removeItem("token");
-        window.location.href = "/login";
+      if (error.request.status == "401") {
+        // localStorage.removeItem("token");
+        // window.location.href = "/login";
+        console.log(error);
       }
       setShowLoader(false);
       toast(error.response.data.message, {
@@ -140,108 +144,6 @@ const Insights = () => {
         theme: "light",
       });
     }
-  };
-
-  const chartData = {
-    series: [
-      {
-        name: "Total Profile Views",
-        data: Data?.click_hits?.hits,
-      },
-      {
-        name: "Total Save Contacts",
-        data: Data?.click_hits?.saved_contact,
-      },
-    ],
-    options: {
-      chart: {
-        height: 350,
-        type: "area",
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        curve: "smooth",
-      },
-      xaxis: {
-        type: "month",
-        categories: [
-          "Jan",
-          "Feb",
-          "Mar",
-          "Apr",
-          "May",
-          "Jun",
-          "Jul",
-          "Aug",
-          "Sep",
-          "Oct",
-          "Nov",
-          "Dec",
-        ],
-      },
-      tooltip: {
-        x: {
-          format: "dd/MM/yy HH:mm",
-        },
-      },
-    },
-  };
-  const chartData2 = {
-    series: dSet || [],
-    options: {
-      chart: {
-        type: "bar",
-        height: 350,
-      },
-      plotOptions: {
-        bar: {
-          horizontal: false,
-          columnWidth: "55%",
-          endingShape: "rounded",
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        show: true,
-        width: 2,
-        colors: ["transparent"],
-      },
-      xaxis: {
-        categories: [
-          "Jan",
-          "Feb",
-          "Mar",
-          "Apr",
-          "May",
-          "Jun",
-          "Jul",
-          "Aug",
-          "Sep",
-          "Oct",
-          "Nov",
-          "Dec",
-        ],
-      },
-      yaxis: {
-        title: {
-          text: "$ (thousands)",
-        },
-      },
-      fill: {
-        opacity: 2,
-      },
-      tooltip: {
-        y: {
-          formatter: function (val) {
-            return "$ " + val + " thousands";
-          },
-        },
-      },
-    },
   };
 
   return token ? (
