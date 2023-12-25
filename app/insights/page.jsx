eb1f329eeb49b4ef61137e8ecd34c32379c04f91
AshwinGoutam77 <ashwini@popipro.com>
2023-12-25 13:58:25 +0530

    live16

--- app/insights/page.jsx
+++ app/insights/page.jsx
@@ -28,7 +28,6 @@ import { redirect } from "next/navigation";
 import { useAuthContext } from "@context/AuthContext";
 import { Chart, registerables } from "chart.js";
 Chart.register(...registerables);
-import ReactApexChart from "react-apexcharts";
 
 const Insights = () => {
   const { token } = useAuthContext();
@@ -130,116 +129,6 @@ const Insights = () => {
         theme: "light",
       });
     }
-  };
-
-  let dSet =
-    Data &&
-    Data?.click_hits?.social_media?.map((item) => {
-      return {
-        name: item?.name,
-        data: item?.data,
-      };
-    });
-  const chartData = {
-    series: [
-      {
-        name: "Total Profile Views",
-        data: Data?.click_hits?.hits,
-      },
-      {
-        name: "Total Save Contacts",
-        data: Data?.click_hits?.saved_contact,
-      },
-    ],
-    options: {
-      chart: {
-        height: 350,
-        type: "area",
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        curve: "smooth",
-      },
-      xaxis: {
-        type: "month",
-        categories: [
-          "Jan",
-          "Feb",
-          "Mar",
-          "Apr",
-          "May",
-          "Jun",
-          "Jul",
-          "Aug",
-          "Sep",
-          "Oct",
-          "Nov",
-          "Dec",
-        ],
-      },
-      tooltip: {
-        x: {
-          format: "dd/MM/yy HH:mm",
-        },
-      },
-    },
-  };
-  const chartData2 = {
-    series: dSet || [],
-    options: {
-      chart: {
-        type: "bar",
-        height: 350,
-      },
-      plotOptions: {
-        bar: {
-          horizontal: false,
-          columnWidth: "55%",
-          endingShape: "rounded",
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        show: true,
-        width: 2,
-        colors: ["transparent"],
-      },
-      xaxis: {
-        categories: [
-          "Jan",
-          "Feb",
-          "Mar",
-          "Apr",
-          "May",
-          "Jun",
-          "Jul",
-          "Aug",
-          "Sep",
-          "Oct",
-          "Nov",
-          "Dec",
-        ],
-      },
-      yaxis: {
-        title: {
-          text: "$ (thousands)",
-        },
-      },
-      fill: {
-        opacity: 2,
-      },
-      tooltip: {
-        y: {
-          formatter: function (val) {
-            return "$ " + val + " thousands";
-          },
-        },
-      },
-    },
   };
 
   return token ? (
@@ -373,13 +262,13 @@ const Insights = () => {
 
             {/* Chart */}
 
-            <div className="row w-100 m-0">
+            {/* <div className="row w-100 m-0">
               <div className="col-12 col-lg-6 mt-4 px-0">
                 <div className="barchart-div mx-4">
                   <div id="chart">
                     <ReactApexChart
-                      options={chartData.options}
-                      series={chartData.series}
+                      options={chartData?.options}
+                      series={chartData?.series}
                       type="area"
                       height={350}
                     />
@@ -390,8 +279,8 @@ const Insights = () => {
                 <div className="col-12 col-lg-6 mt-4 px-0">
                   <div className="barchart-div mx-4">
                     <ReactApexChart
-                      options={chartData2.options}
-                      series={chartData2.series}
+                      options={chartData2?.options}
+                      series={chartData2?.series}
                       type="bar"
                       height={350}
                     />
@@ -400,14 +289,14 @@ const Insights = () => {
               ) : (
                 ""
               )}
-            </div>
+            </div> */}
 
             {/* Product */}
 
             <h5 className="first-title title__separate mx-4 mt-4 text-black">
               {UserData?.titles?.card_products?.visible_name} Analytics
             </h5>
-            <div className="mx-2 mb-4">
+            <div className="mx-4 mb-4">
               <div className="row m-0">
                 <div className="col-12 col-lg-3 margin-sm-top">
                   <div className="card p-4">
