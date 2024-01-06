50e46d4a90186bc9b17440507256909458531eec
ashwin <ashwin@gmail.com>
2024-01-06 22:29:57 +0530

    qr issue fixed

--- components/ViewPages/Header.jsx
+++ components/ViewPages/Header.jsx
@@ -728,14 +728,14 @@ const Header = ({
                 onClick={() => handleShowContactQr()}
                 className={ShowDownloadQr ? "color-black" : ""}
               >
-                Contact Qr
+                Contact QR
               </p>
               <span className="ml-2 mr-2">|</span>
               <p
                 onClick={() => handleShowProfileQr()}
                 className={ShowProfileQr ? "color-black" : ""}
               >
-                Profile Qr
+                Profile QR
               </p>
             </div>
 
