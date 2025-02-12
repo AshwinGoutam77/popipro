abccee80f2103cd5a3002e240efe3def24fdace5
Ashwingoutam77 <ashwini@popipro.com>
2025-02-12 10:44:23 +0530

    latest code

--- app/edit/[profile]/page.jsx
+++ app/edit/[profile]/page.jsx
@@ -111,7 +111,7 @@ const Page = () => {
   }
   return token ? (
     <>
-      {/* {pathname && pathname.includes(pathname.split("/")[1]) && <ToastContainer
+      {pathname && pathname.includes(pathname.split("/")[1]) && <ToastContainer
         position="top-right"
         autoClose={5000}
         hideProgressBar={false}
@@ -122,7 +122,7 @@ const Page = () => {
         draggable
         pauseOnHover
         theme="light"
-      />} */}
+      />}
       {cardData ? (
         <>
           <EditBanner
